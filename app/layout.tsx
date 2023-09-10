806ed35b86c81b5c4e565334b5a640ce1641fea8
Raknage <68710615+Raknage@users.noreply.github.com>
2023-09-10 17:45:15 +0300

    styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { relative } from "path";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,14 +10,14 @@ export const metadata: Metadata = {
   description: "Stuff I do",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} relative bg-gray-700 text-orange-100`}>
+        <div className="absolute right-[0] top-[6rem] -z-10 h-1/5 w-1/2 rounded-full bg-orange-400 blur-[10rem] md:right-[10%] md:w-1/3"></div>
+        <div className="absolute left-[0] top-[0rem] -z-10 h-1/5 w-1/2 rounded-full bg-teal-400 blur-[10rem] md:left-[10%] md:w-1/3"></div>
+        {children}
+      </body>
     </html>
   );
 }
