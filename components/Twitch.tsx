435532fd72aed9e8305804d80deacf4dbe2af3f0
Raknage <68710615+Raknage@users.noreply.github.com>
2023-09-16 00:19:35 +0300

    fixed Twitch iframe

--- components/Twitch.tsx
+++ components/Twitch.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 export default function Twitch() {
   return (
-    <iframe
-      src="https://player.twitch.tv/?raknage&parent=localhost&autoplay=true&muted=true"
-      height="300"
-      width="400"
-      allowFullScreen
-    />
+    <section className="my-32 flex justify-center">
+      <iframe
+        src="https://player.twitch.tv/?channel=raknage&parent=localhost&autoplay=true&muted=true"
+        height="300"
+        width="400"
+        allowFullScreen
+      />
+    </section>
   );
 }
